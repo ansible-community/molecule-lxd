35829a5bceb473dc0bea5bcda9e3ca77ae7c6690
Sorin Sbarnea <ssbarnea@redhat.com>
2020-10-29 16:27:15 +0000

    Made driver compatible with molecule 3.2

--- molecule_lxd/driver.py
+++ molecule_lxd/driver.py
@@ -113,7 +113,7 @@ class LXD(Driver):
         pass
 
     def template_dir(self):
-        """ Return path to its own cookiecutterm templates. It is used by init
+        """Return path to its own cookiecutterm templates. It is used by init
         command in order to figure out where to load the templates from.
         """
         return os.path.join(os.path.dirname(__file__), "cookiecutter")
